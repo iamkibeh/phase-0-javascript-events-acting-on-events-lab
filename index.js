79750693fd0f0b03ef14856f8ced968751811a6a
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-04 22:46:02 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerRight() {
+    
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+if(left <360){
+    dodger.style.left = `${left + 1}px`;
+}
+ }
+// moveDodgerRight()
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+if(left > 0){
+    dodger.style.left = `${left - 1}px`;
+}
+}
+// moveDodgerLeft()
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+      }
+      else if (event.key === "ArrowRight"){
+moveDodgerRight()
+      }
+  });
